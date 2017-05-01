b49645133309f19a42ca9e267797defa02705f28
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-01 14:24:12 -0400

    Create ajax requests to count and time

--- ajax.js
+++ ajax.js
@@ -11,20 +11,43 @@ $(document).ready(function () {
 
   $('#step-3').on('click', function(){
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/pong',
+      url: 'http://first-ajax-api.herokuapp.com/ping',
       method:'GET',
       // data: ,
       dataType: 'text'
     }).done(function(responseData){
-      console.log(responseData);
+      console.log('Request Successful');
       $('#step3456').append(responseData);
     }).fail(function(){
       $('#step3456').append('sorry, I will try harder next time')
+      console.log('Request Failed');
     }).always(function(){
-      console.log('Hey the request finished!');
+      console.log('Request may or may not have been Successful');
     });
   });
 
+  $('#step-7').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method:'GET',
+      // data: ,
+      dataType: 'text'
+    }).done(function(responseData){
+      console.log('Request Successful');
+      $('#step7').append(responseData);
+    });
+  });
+
+  $('#step-8').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method:'GET',
+      data: { timezone: 'Europe/Sofia' },
+      dataType: 'text'
+    }).done(function(responseData){
+      $('#step8').append(responseData);
+    });
+  });
 
 });
 
